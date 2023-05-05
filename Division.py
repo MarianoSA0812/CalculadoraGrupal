ae989549a434596eb97177d0ee064c79bf1b9728
tobiasgordillo <129461030+tobiasgordillo@users.noreply.github.com>
2023-05-04 22:45:31 -0300

    Creado con Colaboratory

---
+++ Division.py
@@ -0,0 +1,66 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMzTviOeCv7iFIOF0/VVmoj",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/MarianoSA0812/CalculadoraGrupal/blob/Division/Division.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "2nTFU7AkAE4r"
+      },
+      "outputs": [],
+      "source": [
+        "def division(a,b):\n",
+        " division= a/b\n",
+        " return division"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print(\"la division es: \",division(3,2))"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "xMy6_H4iFJ4m",
+        "outputId": "e1daf7da-0e87-40c0-bb99-c34834551c72"
+      },
+      "execution_count": null,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "la division es:  1.5\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
